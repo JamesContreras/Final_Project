669ab199a69c88910bf704cb80bb2a2e3e499081
James Contreras <james.a.contreras@outlook.com>
2020-05-17 13:26:30 -0400

    world

---
+++ Controllers/WorldController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Final_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Final_Project.Controllers
+{
+    public class WorldController : Controller
+    {
+        private readonly ILogger<WorldController> _logger;
+
+        public WorldController(ILogger<WorldController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: /<controller>/
+        public IActionResult WorldPage()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
