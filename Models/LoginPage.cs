35a1eb9ed96773d30f53b5f9826bbd19cd80e1f8
James Contreras <james.a.contreras@outlook.com>
2020-05-17 10:50:30 -0400

    new controller and views

---
+++ Models/LoginPage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Final_Project.Models
+{
+    public class LoginPage
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class LoginDBContext : DbContext
+    {
+
+    }
+}
