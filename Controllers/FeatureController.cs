1d90272a22473a886b5a29a61fa3a64c68edd970
James Contreras <james.a.contreras@outlook.com>
2020-05-17 12:59:58 -0400

    feature page

--- Controllers/FeatureController.cs
+++ Controllers/FeatureController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Final_Project.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +13,25 @@ namespace Final_Project.Controllers
 {
     public class FeatureController : Controller
     {
+
+        private readonly ILogger<FeatureController> _logger;
+
+        public FeatureController(ILogger<FeatureController> logger)
+        {
+            _logger = logger;
+        }
+
+
         // GET: /<controller>/
         public IActionResult Feature()
         {
             return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
